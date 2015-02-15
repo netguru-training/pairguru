a11ca78ea15071a5dee9ba829132521833f850ce
Michał Kaźmierczak <michal.kazmierczak@netguru.co>
2015-02-15 17:47:52 +0100

    add movies seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,22 @@
-puts 'Creating genres...'
+require 'faker'
 
+puts 'Creating genres...'
 %w(Action Comedy Sci-Fi War Crime
    Horror Sport Western Drama
    Musicial Romance Thriller
 ).each do |genre|
-  Genre.find_or_create_by(name: genre)
+  Genre.find_or_create_by!(name: genre)
+end
+
+puts 'Creating movies...'
+genre_ids = Genre.pluck(:id)
+100.times do
+  title_how_many_words = [1, 2, 3].sample
+  Movie.find_or_create_by!(
+    title: Faker::Lorem.words(title_how_many_words).join(' ').capitalize,
+    description: Faker::Lorem.paragraph(5),
+    avatar: Faker::Avatar.image,
+    genre_id: genre_ids.sample,
+    released_at: Faker::Date.between(40.years.ago, Date.today)
+  )
 end
