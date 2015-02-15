03e465b9b3407dec258f4652ed56dba501b6cd93
Michał Kaźmierczak <michal.kazmierczak@netguru.co>
2015-02-15 16:33:59 +0100

    add sample genres

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts 'Creating genres...'
+
+%w(Action Comedy Sci-Fi War Crime
+   Horror Sport Western Drama
+   Musicial Romance Thriller
+).each do |genre|
+  Genre.find_or_create_by(name: genre)
+end
