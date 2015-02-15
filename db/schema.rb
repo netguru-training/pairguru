32de5b1bfe92f0388b439a74980eb730a2dc8811
Michał Kaźmierczak <michal.kazmierczak@netguru.co>
2015-02-15 16:21:38 +0100

    introduce genres model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150214221437) do
+ActiveRecord::Schema.define(version: 20150215151704) do
+
+  create_table "genres", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
