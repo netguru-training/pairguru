0cf2101518c6b81c8cf6b5db248df93acb571ae6
Michał Kaźmierczak <michal.kazmierczak@netguru.co>
2015-02-24 11:55:29 +0100

    add factory for movie and genre

---
+++ spec/factories/genre.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :genre do
+    name { Faker::Lorem.word }
+
+    trait :with_movies do
+      after(:create) do |genre|
+        create_list :movie, 5, genre: genre
+      end
+    end
+  end
+end
---
+++ spec/factories/movie.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :movie do
+    title { Faker::Lorem.word }
+    description { Faker::Lorem.sentence(3, true) }
+    released_at { Faker::Date.between(40.years.ago, Date.today) }
+    genre
+  end
+end
