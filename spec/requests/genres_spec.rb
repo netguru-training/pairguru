60ffe0fd5d93fb5079065f18ba18e602c5fbfe5e
Michał Kaźmierczak <michal.kazmierczak@netguru.co>
2015-02-24 11:57:32 +0100

    add some specs

---
+++ spec/requests/genres_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe 'Genres requests', type: :request do
+  let!(:genres) { create_list(:genre, 5, :with_movies) }
+
+  describe 'genre list' do
+    it 'displays only related movies' do
+      visit '/genres/' + genres.sample.id.to_s + '/movies'
+      expect(page).to have_selector('table tr', count: 5)
+    end
+  end
+
+end
---
+++ spec/requests/movies_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe 'Movies requests', type: :request do
+
+  describe 'movies list' do
+    it 'displays right title' do
+      visit '/movies'
+      expect(page).to have_selector('h1', exact: 'Movies')
+    end
+  end
+
+end
