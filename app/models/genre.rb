40b24d8cb519eaa6e5d83aa51a81a6e02a1c2cd5
Michał Kaźmierczak <michal.kazmierczak@netguru.co>
2015-02-24 11:55:52 +0100

    add annotations

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: genres
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Genre < ActiveRecord::Base
   has_many :movies
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: movies
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  released_at :datetime
+#  avatar      :string
+#  genre_id    :integer
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Movie < ActiveRecord::Base
   belongs_to :genre
 end
