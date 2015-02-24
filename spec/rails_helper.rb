91e81a7035b86347fc4b59565e1bfe49eac4e58d
Michał Kaźmierczak <michal.kazmierczak@netguru.co>
2015-02-24 11:59:05 +0100

    add specs config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,9 @@ require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 require 'pry'
+require 'capybara/rails'
+require 'simplecov'
+SimpleCov.start 'rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -29,6 +32,7 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.include FactoryGirl::Syntax::Methods
+  config.include Capybara::DSL
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
