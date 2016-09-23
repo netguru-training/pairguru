cce0394b72248a0a34dd69ce6169c841d56e3c80
Jakub Kubacki <kubacki.jk@gmail.com>
2016-09-23 13:19:06 +0200

    Send emails on registration
    Add confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   validates :phone_number, format: { with: /[+]?\d+(?>[- .]\d+)*/, allow_nil: true }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150310172740) do
+ActiveRecord::Schema.define(version: 20160923104958) do
 
   create_table "genres", force: :cascade do |t|
     t.string   "name"
