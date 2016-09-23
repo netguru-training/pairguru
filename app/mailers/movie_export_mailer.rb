1e2ce2d6a42b78bb30b5b639e6282fcd7321bd69
Jakub Kubacki <kubacki.jk@gmail.com>
2016-09-23 15:30:36 +0200

    Exporter mail

---
+++ app/mailers/movie_export_mailer.rb
@@ -0,0 +1,6 @@
+class MovieExportMailer < ApplicationMailer
+  def send_file(user, file_path)
+    attachments['movies.csv'] = File.read(file_path)
+    mail(to: user.email, subject: "Your export is ready")
+  end
+end
