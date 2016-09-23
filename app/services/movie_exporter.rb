784058b407ea5f11ee0405814c3e54154d8f1853
Jakub Kubacki <kubacki.jk@gmail.com>
2016-09-23 15:30:18 +0200

    Add csv exporter - long running task

---
+++ app/services/movie_exporter.rb
@@ -0,0 +1,23 @@
+require 'csv'
+
+class MovieExporter
+  def call(user, file_path)
+    CSV.open(file_path, 'wb', csv_options) do |csv|
+      Movie.all.each do |movie|
+        csv << [movie.title, movie.description]
+      end
+    end
+    sleep(5) # this emulates long export, do not remove
+    MovieExportMailer.send_file(user, file_path).deliver_now
+  end
+
+  private
+
+  def csv_options
+    {
+      write_headers: true,
+      headers: %w(title description),
+      col_sep: ';'
+    }
+  end
+end
