3e07667c1f518e8811df2d9d620812c52daa6f10
Jakub Kubacki <kubacki.jk@gmail.com>
2017-04-03 18:34:05 +0200

    Fix capybara deprecation in request spec

--- spec/requests/movies_spec.rb
+++ spec/requests/movies_spec.rb
@@ -4,7 +4,7 @@ describe "Movies requests", type: :request do
   describe "movies list" do
     it "displays right title" do
       visit "/movies"
-      expect(page).to have_selector("h1", exact: "Movies")
+      expect(page).to have_selector("h1", text: "Movies")
     end
   end
 end
