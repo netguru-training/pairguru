c8b63e654aabf30f21c0e22c6c2932e6af8135db
Paweł Szyszka <pawelszyszka26@wp.pl>
2020-03-09 22:11:00 +0100

    Creates methods in application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def comments_present?(comments)
+    comments.present?
+  end
+
+  def user_has_no_comments?(comments)
+    users_ids = comments.pluck(:user_id)
+    users_ids.include?(current_user&.id) ? false : true
+  end
+
+  def user_comment?(comment)
+    current_user&.id == comment&.user_id
+  end
+
+  def avatar_or_cover(movie)
+    movie&.avatar.present? ? movie.avatar : movie.cover
+  end
+
+  def is_model_instance?(model, model_name)
+    model.is_a?(model_name)
+  end
 end
