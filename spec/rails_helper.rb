19bafd3b00efd49e34cb594b024b0e567fdf1cf2
Paweł Szyszka <pawelszyszka26@wp.pl>
2020-03-09 21:55:03 +0100

    Adds database cleaner and controller helpers to rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -53,6 +53,20 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+  config.include Devise::Test::ControllerHelpers, type: :controller
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
 
 Shoulda::Matchers.configure do |config|
