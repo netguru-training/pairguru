558add2ffe50cf9c929510c56467002016b8f1ec
annawojcieszek <wojcieszek.ania@gmail.com>
2021-01-11 14:21:42 +0100

    Task 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,19 @@
 #  reset_password_token   :string
 #  reset_password_sent_at :datetime
 #  remember_created_at    :datetime
-#  sign_in_count          :integer          default("0"), not null
+#  sign_in_count          :integer          default(0), not null
 #  current_sign_in_at     :datetime
 #  last_sign_in_at        :datetime
 #  current_sign_in_ip     :string
 #  last_sign_in_ip        :string
 #  created_at             :datetime
 #  updated_at             :datetime
+#  phone_number           :string
+#  confirmation_token     :string
+#  confirmed_at           :datetime
+#  confirmation_sent_at   :datetime
+#  unconfirmed_email      :string
+#  name                   :string           default(""), not null
 #
 
 class User < ApplicationRecord
@@ -23,5 +29,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
+  has_many :comments
+
   validates :phone_number, format: { with: /\A[+]?\d+(?>[- .]\d+)*\z/, allow_nil: true }
+
+  scope :comments_count_ordered, -> { joins(:comments).where('comments.created_at > ?', Date.today - 7.days)
+                                          .group(:id).order('COUNT(comments.id) DESC').limit(10) }
 end
